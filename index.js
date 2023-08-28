f1dc057275d31f92db17b7dcb0a4d5e4afc879d2
João Tavares <joaotavares@MacBook-Pro-de-Joao.local>
2023-08-28 17:26:05 -0300

    adding updates to broadway.json 'a' key

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Command } = require('commander');
+const { name, version } = require('./package.json');
+const { Lantern } = require('lantern');
+const broadway = require('./src/broadway.json');
+const program = new Command();
+
+program
+  .name(name)
+  .version(version);
+
+program.command('parse')
+  .description('Parse a input into its own reference keyword')
+  .argument('<string>', 'string to parse')
+  .option('-d, --debug', 'debug')
+  .action((s, { debug }) => {
+    const p = new Lantern(s, debug);
+    let result = [];
+
+    p.result.forEach((_h_word) => {
+      if (_h_word._children.length === 3) {
+        let w = _h_word._children[2];
+
+        result.push({ word: broadway[_h_word.wide][w],  _children: _h_word });
+      } else {
+        result.push({ word: broadway[_h_word.wide]._default,  _children: _h_word });
+      }
+    });
+
+    console.log(result);
+  });
+
+
+program.parse();
